381612c4fecae7925f7580f6f54caad513c0d0b1
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2022-11-14 21:52:18 +0600

    Initial Commit

---
+++ TableWithHero's/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableWithHero's
+//
+//  Created by Alibek Kozhambekov on 14.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
